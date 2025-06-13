6d2f5f61c59a6be8a31f8393e31675717c3c96d8
pxxja <poojabhambid2004@gmail.com>
2025-06-13 23:39:15 +0530

    create Navbar

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import {assets} from '../assets/assets'
 const Navbar = () => {
   return (
-    <div>
-      <img src={assets.clothsyLogo} alt="" />
-      <button>Logout</button>
+    <div className='flex items-center py-2 px-[4%] justify-between'>
+      <img className='w-[max(10%,80px)]' src={assets.clothsyLogo} alt="" />
+      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-sm sm:text-sm'>Logout</button>
     </div>
   )
 }
