5b136e1717ca239f8d25c40027213d1485b73b0a
pxxja <poojabhambid2004@gmail.com>
2025-06-17 19:39:15 +0530

    completed list items feature admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,6 +10,7 @@ import { ToastContainer } from 'react-toastify';
 
 
 export const backendUrl= import.meta.env.VITE_BACKEND_URL; // Importing backend URL from environment variables
+export const currency= '$';
 
 const App = () => {
   // const [token,setToken] = useState('');
@@ -42,7 +43,7 @@ const App = () => {
           <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
             <Routes>
               <Route path='/add' element={<Add token={token}/>}/>
-              <Route path='/add' element={<List token={token}/>}/>
+              <Route path='/list' element={<List token={token}/>}/>
               <Route path='/orders' element={<Orders token={token}/>}/>
             </Routes>
           </div>
